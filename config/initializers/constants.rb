3ac728d1e89a771982ed6c9dfa77a4bc244cf4eb
jamescarney3 <jmcarneyiii@gmail.com>
2019-10-28 23:23:48 -0400

    Resolve constants initializer warnings

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,13 +1,13 @@
 require 'yaml'
 
-# declare global var to store admin user Twitter IDs
-ADMIN_IDS = []
-
 # try to read IDs from git ignored config file and set
-# var to parsed IDs array
+# global constant to parsed IDs array; if anything goes
+# wrong make it an empty array and we just won't be able
+# to sign anyone in
 begin
   ADMIN_IDS = YAML.load File.read '.admins.yml'
 rescue
+  ADMIN_IDS = []
   puts <<-MESSAGE
     Please provide a whitelist of Twitter IDs to reference during
     admin level user creation in an array at the top level of:
