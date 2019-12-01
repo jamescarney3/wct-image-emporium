97c8be6e907d46b1bc15480710d85121b99201aa
jamescarney3 <jmcarneyiii@gmail.com>
2019-11-30 21:58:48 -0500

    fix image tests to actually check initialization with an asset

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,18 +1,19 @@
 require 'test_helper'
 require 'fileutils'
 
-# tests for successful initialization with an image asset and persistence if
-# and associated admin record is destroyed aren't implemented here even though
-# it seems like they could be because those functionalities are ultimately the
-# responsibilities of ActiveStorage and the User model unit tests respectively
+# tests for persistence if and associated admin record is destroyed aren't
+# implemented here even though it seems like they could be because those
+# functionalities are ultimately the responsibilities the User model unit
+# tests
 
-# see additional notes on what ActiveStorag is doin under the hood here:
+# see additional notes on what ActiveStorag is doing under the hood here:
 # https://evilmartians.com/chronicles/rails-5-2-active-storage-and-beyond
 
 class ImageTest < ActiveSupport::TestCase
   def setup
     @valid_image = images :valid_image
     @no_admin_image = images :no_admin_image
+    @valid_user = users :valid_user
   end
 
   test 'instance_should_validate' do
@@ -26,9 +27,22 @@ class ImageTest < ActiveSupport::TestCase
     assert_not no_title_image.valid?
   end
 
+  test 'can initialize with attached image asset' do
+    file = file_fixture('hoopball.jpg')
+    new_image = Image.create(admin: @valid_user, title: 'da hoopball', asset: { io: file.open, filename: 'HOOPBALL.jpg' })
+
+    assert_not new_image.nil?
+    assert new_image.asset.attached?
+    new_image.asset.open do |attached_file|
+      assert FileUtils.compare_file(file, attached_file)
+    end
+  end
+
   test 'can attach an active storage record' do
     file = file_fixture('hoopball.jpg')
     @valid_image.asset.attach(io: file.open, filename: 'HOOPBALL.jpg')
+
+    assert @valid_image.asset.attached?
     @valid_image.asset.open do |attached_file|
       assert FileUtils.compare_file(file, attached_file)
     end
