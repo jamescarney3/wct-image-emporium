dd7ea7bcfd63298343865004c199e79dc28ec302
jamescarney3 <jmcarneyiii@gmail.com>
2020-03-04 19:22:51 -0500

    Run tags migration & update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_010432) do
+ActiveRecord::Schema.define(version: 2020_03_05_001220) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2019_11_29_010432) do
     t.index ["user_id"], name: "index_images_on_user_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "label", null: false
+    t.string "value", null: false
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "session_token"
@@ -50,4 +59,5 @@ ActiveRecord::Schema.define(version: 2019_11_29_010432) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "images", "users"
+  add_foreign_key "tags", "users"
 end
