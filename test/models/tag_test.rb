b97d59503163561586997bda78cda1a15c42084e
jamescarney3 <jmcarneyiii@gmail.com>
2020-03-04 19:36:09 -0500

    Add test for tag field validation

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
+  def setup
+    @valid_tag = tags :valid_tag
+  end
+
+  test 'instance should validate' do
+    assert @valid_tag.valid?
+    assert_not Tag.new(label: 'Smarf').valid?
+    assert_not Tag.new(value: 'smarf').valid?
+    assert_not Tag.new(label: 'Smarf', value: '').valid?
+    assert_not Tag.new(label: '', value: 'smarf').valid?
+  end
 end
