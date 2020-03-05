04c2812da89e7b9b558cf6fe8657384111e94718
jamescarney3 <jmcarneyiii@gmail.com>
2020-03-04 20:14:52 -0500

    actually add tag fixures,add tag model validations, enhance field validation tests

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  validates :label, presence: true, uniqueness: true
+  validates :value, presence: true, uniqueness: true
+
+  belongs_to :admin, class_name: :User, foreign_key: :user_id, optional: true
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -2,11 +2,17 @@ require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
   def setup
+    @valid_user = users :valid_user
     @valid_tag = tags :valid_tag
+    @valid_tag_no_admin = tags :valid_tag_no_admin
   end
 
   test 'instance should validate' do
     assert @valid_tag.valid?
+    assert @valid_tag_no_admin.valid?
+
+    assert_not Tag.new(label: @valid_tag.label, value: 'shaqtin').valid?
+    assert_not Tag.new(label: 'Shaqtin', value: @valid_tag.value).valid?
     assert_not Tag.new(label: 'Smarf').valid?
     assert_not Tag.new(value: 'smarf').valid?
     assert_not Tag.new(label: 'Smarf', value: '').valid?
