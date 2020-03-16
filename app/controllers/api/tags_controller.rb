20180b7893008eff9070782dd3e91711e3bca586
jamescarney3 <jmcarneyiii@gmail.com>
2020-03-16 12:47:07 -0600

    add tag helper module and index, create tag controller methods

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -4,10 +4,20 @@ class Api::TagsController < ApplicationController
 
   # show me all the tags
   def index
+    render json: Tag.all.map(&helpers.method(:filter_tag_attrs))
   end
 
   # create a new tag record with parameters
   def create
+    @tag = Tag.new(tag_params)
+    @tag.admin = current_user
+    if @tag.save
+      render json: helpers.filter_tag_attrs(@tag), status: 200
+    elsif @tag.errors.messages[:value].include? 'has already been taken' # there's probably a better way to do this
+      render json: { error: '409 conflict, duplicate record' }, status: 409
+    else
+      render json: { error: '422 unprocessable entity' }, status: 422
+    end
   end
 
   # destroy a tag record
@@ -21,4 +31,10 @@ class Api::TagsController < ApplicationController
   # update a tag record
   def update
   end
+
+  private
+
+    def tag_params
+      params.require(:tag).permit(:label, :value)
+    end
 end
