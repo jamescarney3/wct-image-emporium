3656ab55878cbe95827cdb7424340bfcc2c34fec
jamescarney3 <jmcarneyiii@gmail.com>
2020-03-16 13:12:38 -0600

    add tag controller test cases for deletion & required fixtures

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class TagsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @valid_user = users :valid_user
+    @alternate_valid_user = users :alternate_valid_user
+    @valid_tag = tags :valid_tag
+    @valid_tag_no_admin = tags :valid_tag_no_admin
   end
 
   def filter_attrs(tag)
@@ -67,5 +70,51 @@ class TagsControllerTest < ActionDispatch::IntegrationTest
 
     post api_tags_url, params: { tag: replace(tag_params, { label: '' }) }
     assert_response 422
+  end
+
+  test 'destroy tag belonging to logged in user' do
+    log_in! @valid_user
+    delete api_tag_url(@valid_tag.id)
+
+    assert_not Tag.exists?(@valid_tag.id)
+    assert_equal @response.body, filter_attrs(@valid_tag).to_json
+    assert_response 200
+  end
+
+  test 'destroy tag belonging to another user' do
+    log_in! @alternate_valid_user
+    delete api_tag_url(@valid_tag.id)
+
+    assert Tag.exists?(@valid_tag.id)
+    assert_response 403
+  end
+
+  test 'destroy tag belonging to no user' do
+    log_in! @valid_user
+    delete api_tag_url(@valid_tag_no_admin.id)
+
+    assert_not Tag.exists?(@valid_tag_no_admin.id)
+    assert_equal @response.body, filter_attrs(@valid_tag_no_admin).to_json
+    assert_response 200
+  end
+
+  test 'destroy tag when not logged in' do
+    delete api_tag_url(@valid_tag.id)
+
+    assert Tag.exists?(@valid_tag.id)
+    assert_response 401
+  end
+
+  test 'destroy nonexistent tag' do
+    log_in! @valid_user
+    tag_id = rand(42069)
+    until not tags.pluck(:id).include? tag_id do
+      tag_id = rand(42069)
+    end
+    prev_count = Tag.count
+
+    delete api_tag_url(tag_id)
+    assert_equal Tag.count, prev_count
+    assert_response 404
   end
 end
