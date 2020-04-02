21a01943e2c619c7ca9dbdda6b42f8d725f4514d
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-02 11:13:19 -0400

    split relevant part of index into app file

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import 'bootstrap';
+
+import App from './app';
+
 import './styles/main.scss'
 
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Praise be to SMARF</h1>, document.getElementById("root"));
+ReactDOM.render(new App, document.getElementById("root"));
