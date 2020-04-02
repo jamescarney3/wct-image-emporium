2709e7ed261d29780b0ced8ed6921546326275d6
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-02 13:11:49 -0400

    Add router & switch with placeholder routes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <h1>Praise be to SMARF</h1>
+    <Router>
+      <h1>Praise be to SMARF</h1>
+      <a href="/auth/twitter">sign in</a>
+      <Switch>
+        <Route path="/"><div>home page</div></Route>
+        <Route path="/unauthorized"><div>get outa here!</div></Route>
+        <Route path="/admin"><div>welcome, king.</div></Route>
+      </Switch>
+    </Router>
   );
 };
 
