6b67537315042ea3cf5c4e13e82f70319f921e07
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-04 14:18:48 -0400

    add timelord

--- client/src/pages/unauthorized.tsx
+++ client/src/pages/unauthorized.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 
 const Unauthorized: React.FC = () => {
+  const robIdx = Math.floor(Math.random() * Math.floor(3)) + 1;
+  const robPath = `/timelord-block-${robIdx}.jpg`;
+
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-lg-6">
+          <img src={robPath} className="w-100 mt-5" />
           <h2>REJECTED</h2>
           <p>
             Either something went wrong with the authentication process or your
