c5d70551e1aa3e20cafcac02738d1167b355da01
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-06 22:51:06 -0400

    add module and index export for image upload form

---
+++ client/src/components/image-upload-form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ImageUploadForm: React.SFC = () => {
+  return (
+    <form>
+      [[ form placeholder ]]
+    </form>
+  );
+};
+
+export default ImageUploadForm;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from './header';
 export { default as AuthRoute } from './auth-route';
+export { default as ImageUploadForm } from './image-upload-form';
