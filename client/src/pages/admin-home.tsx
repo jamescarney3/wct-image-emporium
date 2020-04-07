a8896f78be2dd4d473e54e63439fdd10bafa28ed
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-06 23:11:31 -0400

    groom admin home page for image upload component

--- client/src/pages/admin-home.tsx
+++ client/src/pages/admin-home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
+import { AuthRoute, ImageUploadForm } from '~/components';
 import { useStore } from '~/context/store';
 
 
-const AdminHome = () => {
+interface IAdminHome {
+  match: { path: string };
+}
+
+const AdminHome: React.SFC<IAdminHome> = ({ match }) => {
 
   const [user, userActionCreators] = useStore('user');
   const { signOut } = userActionCreators;
@@ -15,20 +20,27 @@ const AdminHome = () => {
         <div className="col-lg-3">
           <ul className="list-group">
             <li className="list-group-item">
-              <Link to="/admin">admin home</Link>
+              <Link to={match.path}>admin home</Link>
             </li>
             <li className="list-group-item">
-              <Link to="/admin/images">my images</Link>
+              <Link to={`${match.path}/images`}>my images</Link>
             </li>
             <li className="list-group-item">
-              <Link to="/admin/tags">my tags</Link>
+              <Link to={`${match.path}/tags`}>my tags</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to={`${match.path}/image-upload`}>image upload</Link>
             </li>
             <li className="list-group-item">
               <button className="btn btn-link p-0" onClick={signOut}>log out</button>
             </li>
           </ul>
         </div>
-        <div className="col-lg-9">[[ main content placeholder ]]</div>
+        <div className="col-lg-9">
+          <Switch>
+            <AuthRoute path={`${match.path}/image-upload`} component={ImageUploadForm} />
+          </Switch>
+        </div>
       </div>
     </div>
   );
