b10372aa234958de1d244843296a6fc6577e6352
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-06 23:25:42 -0400

    make provisional form layout

--- client/src/components/image-upload-form.tsx
+++ client/src/components/image-upload-form.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 
 const ImageUploadForm: React.SFC = () => {
   return (
-    <form>
-      [[ form placeholder ]]
+    <form className="mt-lg-5">
+      <div className="form-group">
+        <label htmlFor="image-upload-title">image title</label>
+        <input type="text" className="form-control" name="image-upload-title" placeholder="image title" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="image-upload-file">image file</label>
+        <input type="file" className="form-control-file" name="image-upload-file" />
+      </div>
     </form>
   );
 };
