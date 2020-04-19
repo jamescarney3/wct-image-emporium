ffa7af21ea3dcbeb47540acfa3e18c26845e8fb3
jamescarney3 <jmcarneyiii@gmail.com>
2020-04-19 11:35:30 -0400

    add image scope for tag matching and incorporate in controller idx method

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,6 +9,7 @@ class Image < ApplicationRecord
   has_many :tags, through: :image_tags
 
   scope :for_user, lambda { |user| where(admin: user) }
+  scope :with_tags, lambda { |*tag_ids| find_by_sql generate_tags_query(*tag_ids) }
 
   private
 
@@ -16,5 +17,18 @@ class Image < ApplicationRecord
       if not asset.attached?
         errors.add :asset, 'must be attached'
       end
+    end
+
+    def self.generate_tags_query(*ids)
+      "
+        SELECT * FROM images
+        INNER JOIN (
+          SELECT s1images.id, count(s1tags) FROM images AS s1images
+          INNER JOIN image_tags ON s1images.id = image_tags.image_id
+          INNER JOIN tags AS s1tags ON image_tags.tag_id = s1tags.id
+          WHERE s1tags.id IN (#{ids.join(',')}) GROUP BY s1images.id
+        ) AS matched_images ON images.id = matched_images.id
+        WHERE count = #{ids.count};
+      "
     end
 end
